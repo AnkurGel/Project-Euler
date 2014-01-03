aa1a15418b6c30dc5f646ecccca8f7af2c3f3eb0
Ankur Goel <ankurgel@gmail.com>
2014-01-04 00:13:35 +0530

    Maximum path sum II | Extended version of Problem 18

---
+++ Problem67.cpp
@@ -0,0 +1,72 @@
+#include<iostream>
+#include<string>
+#include<stdio.h>
+#include<fstream>
+#include<iomanip>
+using namespace std;
+
+int numeric(string);
+int find_max(int, int);
+int main(){
+
+  int size = 100; //matrix size;
+  /* Acquiring input */
+  ifstream ifile;
+  ifile.open("Problem67-triangle.txt", ifstream::in);
+  string line; int i, j=0, k;
+  string buffer = "";
+  int arr[size][size]; long max[size][size];
+  for(i = 0; i < size; i++) for(k = 0; k < size; k++) {arr[i][k] = 0; max[i][k] = 0; }
+
+  while(ifile.good()){
+    getline(ifile, line);
+    //    ifile.get(); 
+
+    buffer = ""; k = 0;
+    for(i = 0; i < line.length(); i++){
+      if(line[i] != ' '){
+        buffer += line[i];
+      }
+      else{
+        arr[j][k++] = numeric(buffer);
+        buffer = ""; // clear buffer
+      }
+    }
+    arr[j][k] = numeric(buffer);
+    j++;
+  }
+
+  
+  //Dynamic programming approach:
+  //Set maximum paths, starting from last row - boundary case:
+  for(i = 0; i < size; i++) max[size-1][i] = arr[size-1][i];
+
+  //now starting from last 2nd row till first, take two rows at time,
+  //and set corresponding max
+  for(i = size - 2, k = 1; i >= 0; i--, k++){
+    for(j = 0; j < size-k; j++){
+      max[i][j] = arr[i][j] + find_max(max[i+1][j], max[i+1][j+1]);
+    }
+  }
+
+  cout << "\n\nMAX matrix " << endl;
+  for(i = 0; i < size; i++){
+    for(j = 0; j < size; j++)
+      cout << setw(2) << max[i][j] << " ";
+    cout << endl;
+  }
+
+  cout << "\nRESULT: " << max[0][0];
+  return 0;
+}
+
+int find_max(int a, int b){
+  return a > b ? a : b;
+}
+int numeric(string s){
+  int num = 0;
+  for(int i = 0; i < s.length(); i++){
+    num = num * 10 + s[i] - 48;
+  }
+  return num;
+}
